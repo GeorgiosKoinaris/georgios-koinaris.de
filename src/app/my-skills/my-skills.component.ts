3ceb8e9e2a5363b397d437d69539004eaae2eb92
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-05-04 10:44:39 +0200

    first style my-skills-component

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
-
+  icons = [
+    './assets/img/skills/angular.png',
+    './assets/img/skills/typescript.png',
+    './assets/img/skills/javascript.png',
+    './assets/img/skills/html.png',
+    './assets/img/skills/firebase.png',
+    './assets/img/skills/git.png',
+    './assets/img/skills/css.png',
+    './assets/img/skills/api.png',
+    './assets/img/skills/scrum.png',
+    './assets/img/skills/material-design.png',
+  ]
+  iconTexts = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'Firebase',
+    'GIT',
+    'CSS',
+    'Rest-Api',
+    'Scrum',
+    'Material design'
+  ]
 }
