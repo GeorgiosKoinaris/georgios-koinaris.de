1ab321cbe10fe6f183b241edcde53918dde3f5dc
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-05-16 16:56:52 +0200

    first contact-section style finished

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,5 +7,7 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  nameFormControl = new FormControl('', [Validators.required]);
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  messageFormControl = new FormControl('', [Validators.required]);
 }
