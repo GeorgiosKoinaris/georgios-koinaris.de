92f86a93c964ba3e6556bedc328ec9b410c7aacd
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-05-24 18:04:05 +0200

    scroll disabled when responsive menu open

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private viewportScroller: ViewportScroller) {}
+  constructor(
+    private viewportScroller: ViewportScroller,
+    private renderer: Renderer2
+  ) {}
 
   scrollToTop() {
     this.viewportScroller.scrollToPosition([0, 0]);
   }
   navbarOpen = false;
-  
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+
+    if (this.navbarOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+    }
   }
 }
