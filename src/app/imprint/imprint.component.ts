c73136219ec6aecdc77f31a233769be8fbcd6525
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-05-30 12:43:05 +0200

    first changes for publish

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
   templateUrl: './imprint.component.html',
-  styleUrls: ['./imprint.component.scss']
+  styleUrls: ['./imprint.component.scss'],
 })
 export class ImprintComponent {
+  constructor(private viewportScroller: ViewportScroller) {
+    this.scrollToTop();
+  }
 
+  scrollToTop() {
+    this.viewportScroller.scrollToPosition([0, 0]);
+  }
 }
